b274240eda42dfc874244aff5936f99562c15b40
15895880466 <464836083@qq.com>
2018-04-03 09:21:09 +0800

    commit

--- app/src/main/java/com/example/xiaoyanjun/gittest/MainActivity.java
+++ app/src/main/java/com/example/xiaoyanjun/gittest/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.xiaoyanjun.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
